7573fe4773a3919359aa25c3efae27506f33cb3c
Jonathan Buttner <jonathan.e.buttner@gmail.com>
2018-07-02 19:40:46 -0400

    tests for config json file

---
+++ config.go
@@ -0,0 +1,9 @@
+package main
+
+type Config struct {
+	Key       string `json:"key"`
+	Budget    string `json:"budget"`
+	Category  string `json:"category"`
+	StartDate string `json:"start_date"`
+	EndDate   string `json:"end_date"`
+}
---
+++ config_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+var config = `
+{
+	"key": "FAKE-KEY",
+	"budget": "MyBudget",
+	"category": "RandomCat",
+	"start_date": "2000-01-01",
+	"end_date": "2001-01-01"
+}
+`
+
+func TestConfig(t *testing.T) {
+	var c Config
+	b := []byte(config)
+	if err := json.Unmarshal(b, &c); err != nil {
+		t.Errorf("Unmarshall err: %s", err)
+	}
+
+	if c.Budget != "MyBudget" {
+		t.Errorf("Budget field was wrong: %s", c.Budget)
+	}
+
+	if c.Category != "RandomCat" {
+		t.Errorf("Category field was wrong: %s", c.Category)
+	}
+
+	if c.Key != "FAKE-KEY" {
+		t.Errorf("Key field was wrong: %s", c.Key)
+	}
+
+	if c.StartDate != "2000-01-01" {
+		t.Errorf("StartDate field was wrong: %s", c.StartDate)
+	}
+
+	if c.EndDate != "2001-01-01" {
+		t.Errorf("EndDate field was wrong: %s", c.EndDate)
+	}
+}
