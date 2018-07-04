9edb1557a7ff6cd2753116d4b18ee4817bc857f9
Jonathan Buttner <jonathan.e.buttner@gmail.com>
2018-07-03 22:34:17 -0400

    tests for getting category id

--- utils.go
+++ utils.go
@@ -2,7 +2,7 @@ package main
 
 import "errors"
 
-func GetCategory(client ClientInt, budgetName string, catName string) (string, error) {
+func GetCategoryID(client ClientInt, budgetName string, catName string) (string, error) {
 	budgets, err := client.ListBudgets()
 	if err != nil {
 		return "", err
@@ -20,4 +20,21 @@ func GetCategory(client ClientInt, budgetName string, catName string) (string, e
 		return "", errors.New("Unable to find the budget: " + budgetName)
 	}
 
+	catWithGroup, err := client.ListCategories(budgetID)
+	if err != nil {
+		return "", err
+	}
+
+	var catID string
+	for _, c := range catWithGroup {
+		if c.Name == catName {
+			catID = c.Id
+		}
+	}
+
+	if catID == "" {
+		return "", errors.New("Unable to find the category: " + catName)
+	}
+
+	return catID, nil
 }
--- utils_test.go
+++ utils_test.go
@@ -2,20 +2,31 @@ package main
 
 import (
 	"errors"
+	"strings"
 	"testing"
 
 	"github.com/davidsteinsland/ynab-go/ynab"
 )
 
 type FakeClient struct {
+	listBudErr bool
+	retBud     []ynab.BudgetSummary
+	listCatErr bool
+	retCat     []ynab.CategoryGroupWithCategories
 }
 
-func (f *FakeClient) ListCategories(budgetId string) ([]ynab.CategoryGroupWithCategories, error) {
-	return nil, errors.New("blah")
+func (f *FakeClient) ListCategories(budgetID string) ([]ynab.CategoryGroupWithCategories, error) {
+	if f.listCatErr {
+		return nil, errors.New("error categories")
+	}
+	return f.retCat, nil
 }
 
 func (f *FakeClient) ListBudgets() ([]ynab.BudgetSummary, error) {
-	return nil, errors.New("error budgets")
+	if f.listBudErr {
+		return nil, errors.New("error budgets")
+	}
+	return f.retBud, nil
 }
 
 func (f *FakeClient) GetBudget(id string) (ynab.BudgetDetail, error) {
@@ -23,13 +34,70 @@ func (f *FakeClient) GetBudget(id string) (ynab.BudgetDetail, error) {
 }
 
 func TestGetCategory(t *testing.T) {
-	client := &FakeClient{}
+	d := make([]ynab.BudgetSummary, 1)
+	d[0] = ynab.BudgetSummary{Name: "my-budget", Id: "b-id"}
 
-	c, err := GetCategory(client, "Blah")
+	cat := make([]ynab.CategoryGroupWithCategories, 1)
+	cat[0] = ynab.CategoryGroupWithCategories{
+		CategoryGroup: ynab.CategoryGroup{
+			Name: "my-category",
+			Id:   "c-id",
+		},
+	}
+
+	client := &FakeClient{listBudErr: false, listCatErr: false, retBud: d, retCat: cat}
+
+	c, err := GetCategoryID(client, "my-budget", "my-category")
 	if err != nil {
 		t.Errorf("Error while getting category: %s", err)
 	}
-	if c != "fake-id" {
+	if c != "c-id" {
 		t.Errorf("Category didn't equal fake one: %s", c)
+	}
+}
+
+func TestGetCategoryErr(t *testing.T) {
+	cl := &FakeClient{listBudErr: true}
+
+	_, err := GetCategoryID(cl, "blah", "blah")
+	if err == nil || err.Error() != "error budgets" {
+		t.Error("Expected error budget")
+	}
+	cl = &FakeClient{
+		listCatErr: true,
+		retBud: []ynab.BudgetSummary{
+			ynab.BudgetSummary{
+				Id:   "b-id",
+				Name: "my-budget",
+			},
+		},
+	}
+	_, err = GetCategoryID(cl, "my-budget", "blah")
+
+	if err == nil || err.Error() != "error categories" {
+		t.Errorf("Expected error category: %s", err.Error())
+	}
+}
+
+func TestGetCategoryCantFind(t *testing.T) {
+	cl := &FakeClient{}
+
+	_, err := GetCategoryID(cl, "blah", "blah")
+	if err == nil || !strings.Contains(err.Error(), "Unable to find the budget") {
+		t.Error("Expected budget error")
+	}
+
+	cl = &FakeClient{
+		retBud: []ynab.BudgetSummary{
+			ynab.BudgetSummary{
+				Name: "my-budget",
+				Id:   "b-id",
+			},
+		},
+	}
+
+	_, err = GetCategoryID(cl, "my-budget", "blah")
+	if err == nil || !strings.Contains(err.Error(), "Unable to find the category") {
+		t.Errorf("Expected category error: %s", err.Error())
 	}
 }
